c92127271c1583887cf8d8d00ece567d8aa7f65d
anuragraiofficial321 <anuragraiofficial321@gmail.com>
2023-07-31 18:39:28 +0530

    Resolved all types of data

--- Main.py
+++ Main.py
@@ -3,16 +3,19 @@ import os
 from Secret_key import openai_api_key
 import csv_utils
 import pdf_utils
+import excel_utils
 #from csv_utils import get_answer_csv
 #from pdf_utils import get_answer_pdf
 #os.environ["OPENAI_API_KEY"]=openai_api_key
 
+
+
 def main():
-    st.set_page_config(page_title="Load PDF/CSV/TEXT for semantic search")
-    st.header("PDF/CSV/TEXT Q&A Bot")
+    st.set_page_config(page_title="Semantic Search Q&A Bot", layout="wide")
+    st.header("PDF/CSV/TEXT/EXCEL Q&A Bot")
     #st.markdown("Upload a PDF file/CSV file/Text file and ask questions about its content using Natural Language Processing!")
     #col1, col2 = st.columns(2)
-    pdf_or_csv_or_txt=st.file_uploader("Load: PDF file/CSV file/Text file:",type=["pdf",'csv','txt'])  #,accept_multiple_files=True #for uploaded_file in uploaded_files: bytes_data = uploaded_file.read()
+    pdf_or_csv_or_txt=st.file_uploader("Load: PDF file/CSV file/Text file/EXCEL file:",type=["pdf",'csv','txt',"xlsx"])  #,accept_multiple_files=True #for uploaded_file in uploaded_files: bytes_data = uploaded_file.read()
     if pdf_or_csv_or_txt is not None:
         try:
             file_extension = os.path.splitext(pdf_or_csv_or_txt.name)[1].lower()
@@ -47,12 +50,21 @@ def main():
                 #pdf_file_path = save_uploaded_pdf(pdf)
                 #text=langchain_pdf_totext(pdf)
 
+            elif file_extension in [".xlsx",".xls"]:
+                query=st.text_input("Ask a Question From Uploaded Files")
+                button=st.button("Submit")
+                if button:
+                    print(file_extension)
+                    #print("out pdf")
+                    #st.write(text_content)
+                    st.write(excel_utils.get_answer_excel(pdf_or_csv_or_txt,query))
+                    
             
             
             
 
         except:
-            st.write("Unsupported file type. Please upload a CSV, TXT, or PDF file.")
+            st.error("Error occurred during processing.")
 
         #finally:
             #if pdf_file_path and os.path.exists(pdf_file_path):
